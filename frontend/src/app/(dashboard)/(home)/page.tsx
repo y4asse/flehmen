737e984a55202063c02bcb9838a7e842e14adf8e
Hiroto2002 <hiroto2002h14@icloud.com>
2024-12-03 15:21:46 +0900

    refact

--- frontend/src/app/(dashboard)/(home)/page.tsx
+++ frontend/src/app/(dashboard)/(home)/page.tsx
@@ -1,20 +1,41 @@
 import { Windows } from "@/components/common/Windows";
 import React from "react";
-import { TimeGraph } from "./_components/TimeGraph";
-import { WeekGraph } from "./_components/WeekGraph";
+import { MonthlyGraph } from "./_components/MonthlyGraph";
+import { WeeklyGraph } from "./_components/WeeklyGraph";
 import { Habit } from "./_components/Habit";
+
+export type DateTweetCount = {
+  date: string;
+  count: number;
+};
+
+export type TweetDayCount = {
+  day: string;
+  count: number;
+};
+
+export type WeeklyTweetCount = {
+  day: "げつ" | "にち" | "どー" | "きん" | "もく" | "すい" | "かー";
+  count: number;
+};
 
 const Page = () => {
   const windows = [
     {
       ...timeGraphWindow,
-      title: "何の何時がひまかなー?？",
-      children: <TimeGraph />,
+      title: "いつひまだったの",
+      children: <MonthlyGraph monthlyTweetCounts={monthlyTweetCounts} />,
     },
     {
       ...weekGraphWindow,
-      title: "何に一番ついーとしてる？",
-      children: <WeekGraph />,
+      title: "なんようにひまなんだろ",
+      children: (
+        <WeeklyGraph
+          weeklyAllTweetCounts={weeklyAllTweetCounts}
+          nameKey="day"
+          dataKey="count"
+        />
+      ),
     },
     {
       ...habitWindow,
@@ -29,6 +50,49 @@ const Page = () => {
     </div>
   );
 };
+
+const monthlyTweetCounts: DateTweetCount[] = [
+  { date: "11/01", count: 2 },
+  { date: "11/02", count: 0 },
+  { date: "11/03", count: 5 },
+  { date: "11/04", count: 1 },
+  { date: "11/05", count: 4 },
+  { date: "11/06", count: 0 },
+  { date: "11/07", count: 3 },
+  { date: "11/08", count: 2 },
+  { date: "11/09", count: 0 },
+  { date: "11/10", count: 1 },
+  { date: "11/11", count: 4 },
+  { date: "11/12", count: 0 },
+  { date: "11/13", count: 3 },
+  { date: "11/14", count: 2 },
+  { date: "11/15", count: 0 },
+  { date: "11/16", count: 1 },
+  { date: "11/17", count: 4 },
+  { date: "11/18", count: 0 },
+  { date: "11/19", count: 3 },
+  { date: "11/20", count: 2 },
+  { date: "11/21", count: 0 },
+  { date: "11/22", count: 1 },
+  { date: "11/23", count: 4 },
+  { date: "11/24", count: 0 },
+  { date: "11/25", count: 3 },
+  { date: "11/26", count: 2 },
+  { date: "11/27", count: 0 },
+  { date: "11/28", count: 1 },
+  { date: "11/29", count: 4 },
+  { date: "11/30", count: 0 },
+];
+
+const weeklyAllTweetCounts: WeeklyTweetCount[] = [
+  { day: "げつ", count: 400 },
+  { day: "にち", count: 239 },
+  { day: "どー", count: 189 },
+  { day: "きん", count: 278 },
+  { day: "もく", count: 200 },
+  { day: "すい", count: 300 },
+  { day: "かー", count: 300 },
+];
 
 const busy_color_index_list = [
   [0, 1, 2, 3, 4, 5, 0],
