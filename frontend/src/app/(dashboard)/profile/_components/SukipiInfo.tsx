caca65e2ccf174eff8ac60b3d4c35e9bf3fb1c64
Kipeo <kp225048@gmail.com>
2024-12-05 16:13:13 +0900

    編集機能 スタイル未修正

--- frontend/src/app/(dashboard)/profile/_components/SukipiInfo.tsx
+++ frontend/src/app/(dashboard)/profile/_components/SukipiInfo.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Sukipi } from "../page";
+import { Button } from "@/components/ui/button";
+import { info } from "console";
 
 type Props = {
   sukipi: Sukipi;
+  onUpdate: (updatedSukipi: Sukipi) => void;
 };
 
 // メインコンポーネント
-export const SukipiInfo = ({ sukipi }: Props) => {
+export const SukipiInfo = ({ sukipi, onUpdate }: Props) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [formData, setFormData] = useState<Sukipi>(sukipi);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSave = () => {
+    onUpdate(formData);
+    setIsEditing(false);
+  };
+
+  console.log(formData);
+
   // Sukipiオブジェクトから情報もらう
   const infoItems = [
     { title: "なまえ", content: sukipi.name },
@@ -29,11 +47,45 @@ export const SukipiInfo = ({ sukipi }: Props) => {
     { title: "最寄駅", content: sukipi.nearlyStation || "?まだわからない?" },
   ];
 
+  if (isEditing) {
+    return (
+      <div>
+        <div style={styles.editor}>
+          <div>
+            {infoItems.map((item) => (
+              <div key={item.title} style={styles.title}>
+                {item.title}
+              </div>
+            ))}
+          </div>
+          <div>
+            {Object.keys(formData).map((key) => (
+              <div key={key} style={styles.content}>
+                <input
+                  type="text"
+                  name={key}
+                  value={(formData as any)[key] || ""}
+                  onChange={handleInputChange}
+                  style={styles.editInput}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <button onClick={() => setIsEditing(false)} style={styles.cancel}>
+          もどる
+        </button>
+        <Button onClick={handleSave}>保存</Button>
+      </div>
+    );
+  }
+
   return (
     <div>
       {infoItems.map((item) => (
         <InfoItem key={item.title} title={item.title} content={item.content} />
       ))}
+      <Button onClick={() => setIsEditing(true)}>編集</Button>
     </div>
   );
 };
@@ -62,11 +114,25 @@ const styles: { [key: string]: React.CSSProperties } = {
     fontWeight: "bold",
     color: "white",
   },
+  editor: {
+    display: "flex",
+    margin: "10px 0",
+    fontSize: "0.8em",
+    fontWeight: "bold",
+    color: "white",
+  },
   title: {
     width: "100px",
   },
   content: {
     marginLeft: "10px",
+  },
+  cancel: {
+    color: "white",
+    backgroundColor: "black",
+  },
+  editInput: {
+    background: "transparent",
   },
 };
 
