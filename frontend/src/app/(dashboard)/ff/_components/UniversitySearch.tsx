fdd3b32aee968e3de8bb3b2edac763bf3b519053
Hiroto2002 <hiroto2002h14@icloud.com>
2024-12-05 14:25:21 +0900

    feat:university search

--- frontend/src/app/(dashboard)/ff/_components/UniversitySearch.tsx
+++ frontend/src/app/(dashboard)/ff/_components/UniversitySearch.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+import { Flex } from "@/components/ui/flex";
+import { Search } from "@/components/ui/search";
+import React, { Suspense } from "react";
+import UniversityList from "./UniversityList";
 
-export const UniversitySearch = () => {
-  return <div>UniversitySearch</div>;
+type Props = {
+  query: string;
+};
+
+export const UniversitySearch = (props: Props) => {
+  const { query } = props;
+
+  return (
+    <Flex
+      className="h-full w-full p-4 gap-4"
+      align={"start"}
+      justify={"start"}
+      direction={"column"}
+    >
+      <Search placeholder="略称もいけるよ" />
+      <Suspense fallback={<div>loading...</div>}>
+        <UniversityList query={query} />
+      </Suspense>
+    </Flex>
+  );
 };
