330eeb42c7888e3c42c00fb38b22afaa6f9dec8f
Kipeo <kp225048@gmail.com>
2024-12-06 12:15:34 +0900

    グラフ完成

--- frontend/src/app/(dashboard)/reply/page.tsx
+++ frontend/src/app/(dashboard)/reply/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import ReplyList from "./_components/ReplyList";
 import { Sukipi } from "../profile/page";
 import Contents from "./_components/Contents";
+import { MonthlyRep } from "./_components/MonthlyRep";
 
 export type reply = {
   id: number;
@@ -16,8 +17,18 @@ export type content = {
   content: string;
 };
 
+export type monthly = {
+  date: string;
+  count: number;
+};
+
 const Page = () => {
   const windows = [
+    {
+      ...monthlRep,
+      title: `過去１ヶ月の${sukipi.name}くんのリプの絡み`,
+      children: <MonthlyRep GraphInfo={GraphInfo} />,
+    },
     {
       ...replyList,
       title: `${sukipi.name}くんとリプしてる人リスト`,
@@ -28,11 +39,6 @@ const Page = () => {
       title: "内容",
       children: <Contents repContents={replyContent} />,
     },
-    // {
-    //   ...contents,
-    //   title: `内容`,
-    //   children: <ReplyList replyContent={replyContent} />,
-    // },
   ];
 
   return (
@@ -86,6 +92,39 @@ const replyContent: content[] = [
   },
 ];
 
+const GraphInfo: monthly[] = [
+  { date: "11/01", count: 2 },
+  { date: "11/02", count: 0 },
+  { date: "11/03", count: 5 },
+  { date: "11/04", count: 1 },
+  { date: "11/05", count: 4 },
+  { date: "11/06", count: 0 },
+  { date: "11/07", count: 3 },
+  { date: "11/08", count: 2 },
+  { date: "11/09", count: 0 },
+  { date: "11/10", count: 1 },
+  { date: "11/11", count: 4 },
+  { date: "11/12", count: 0 },
+  { date: "11/13", count: 3 },
+  { date: "11/14", count: 2 },
+  { date: "11/15", count: 0 },
+  { date: "11/16", count: 1 },
+  { date: "11/17", count: 4 },
+  { date: "11/18", count: 0 },
+  { date: "11/19", count: 3 },
+  { date: "11/20", count: 2 },
+  { date: "11/21", count: 0 },
+  { date: "11/22", count: 1 },
+  { date: "11/23", count: 4 },
+  { date: "11/24", count: 0 },
+  { date: "11/25", count: 3 },
+  { date: "11/26", count: 2 },
+  { date: "11/27", count: 0 },
+  { date: "11/28", count: 1 },
+  { date: "11/29", count: 4 },
+  { date: "11/30", count: 0 },
+];
+
 const replyList = {
   initSize: { width: 600, height: 400 },
   initPosition: {
@@ -104,6 +143,14 @@ const contents = {
   },
 };
 
+const monthlRep = {
+  initSize: { width: 800, height: 600 },
+  initPosition: {
+    x: 700,
+    y: 40,
+    z: 1,
+  },
+};
 const sukipi: Sukipi = {
   name: "早瀬",
 };
