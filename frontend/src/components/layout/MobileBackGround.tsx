d139110ab585ffa60d04f372d274abe278844948
Kipeo <kp225048@gmail.com>
2025-02-01 18:33:22 +0900

    mobile bg

--- frontend/src/components/layout/MobileBackGround.tsx
+++ frontend/src/components/layout/MobileBackGround.tsx
@@ -3,28 +3,21 @@ import Image from "next/image";
 import { Flex } from "../ui/flex";
 import Link from "next/link";
 
-const bottomNavItems = [
-  { name: "ホーム", href: "/", icon: "/images/file.svg" },
-  { name: "プロフ", href: "/profile", icon: "/images/file.svg" },
-  { name: "FF", href: "/ff", icon: "/images/file.svg" },
-  { name: "リプ", href: "/reply", icon: "/images/file.svg" },
-  { name: "ひみつ", href: "/secret", icon: "/images/file.svg" },
-];
-
 export const MobileBackGround = () => {
   return (
     <Flex className="bg-black h-[100vh] w-[100vw] absolute top-0 left-0">
-      <div className="fixed bottom-2  flex justify-between w-[95%] bg-zinc-800 px-2 py-2 rounded-sm">
-        {bottomNavItems.map((item) => (
-          <Link
-            href={item.href}
-            key={item.name}
-            className="flex flex-col items-center gap-2"
-          >
-            <Image src={item.icon} alt="icon" width={35} height={35} />
-            <span className="text-white">{item.name}</span>
-          </Link>
-        ))}
+      <Image
+        src="/images/cat_bg.svg"
+        alt="background"
+        width={300} // 画像の幅を指定
+        height={300} // 画像の高さを指定
+        className="z-0"
+        style={{ opacity: 0.3 }}
+      />
+      <div className="fixed bottom-2  flex justify-center w-[95%] px-2 py-2 rounded-sm">
+        <Link href="/">
+          <Image src="/images/logo.svg" alt="icon" width={200} height={200} />
+        </Link>
       </div>
     </Flex>
   );
