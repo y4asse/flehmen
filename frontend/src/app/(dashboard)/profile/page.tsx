5671a97a34a4f25f59da52d5d744a7a1e5791f13
Hayase Taiki <yasse0218@gmail.com>
2025-02-07 01:08:00 +0900

    エラーハンドリング修正

--- frontend/src/app/(dashboard)/profile/page.tsx
+++ frontend/src/app/(dashboard)/profile/page.tsx
@@ -7,7 +7,19 @@ import { MBTI } from "@/types/mbti";
 
 const Page = async () => {
   const { userId } = await auth();
-  const user = await db.select().from(users).where(eq(users.clerkId, userId!));
+  if (!userId) {
+    return <div>ログインしてください</div>;
+  }
+  const user = await db.select().from(users).where(eq(users.clerkId, userId)).catch((error) => {
+    console.error(error);
+    return null;
+  });
+  if (!user) {
+    return <div>エラーが発生しました</div>;
+  }
+  if (user.length === 0) {
+    return <div>ユーザーが見つかりません</div>;
+  }
   const sukipi = (await db.select().from(sukipis).where(eq(sukipis.userId, user[0].id))).map((sukipi) => ({
     name: sukipi.name,
     weight: sukipi.weight,
