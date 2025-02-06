dad139ac092a00e77e597020468578ec732bac77
hayase taiki <yasse0218@gmail.com>
2025-02-07 03:29:04 +0900

    Webhook用のエンドポイントを作成 (#102)

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -2,7 +2,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 import { NextRequest, NextResponse } from 'next/server'
 
 const isOnboardingRoute = createRouteMatcher(['/onboarding'])
-const isPublicRoute = createRouteMatcher([])
+const isPublicRoute = createRouteMatcher(['/api/clerk'])
 
 export default clerkMiddleware(async (auth, req: NextRequest) => {
   const { userId, sessionClaims, redirectToSignIn } = await auth()
